6b54b1c60a04680f95c1826929dd189ee4133782
Alex <surge@aegirs-iMac.local>
2016-01-30 10:14:56 +1000

    Create the Home base and Base Layout

---
+++ cwd/assets/js/main.js
@@ -0,0 +1,12 @@
+/*======================================
+=            Dropdown js           =
+======================================*/
+  $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+      event.preventDefault(); 
+      event.stopPropagation(); 
+      $(this).parent().siblings().removeClass('open');
+      $(this).parent().toggleClass('open');
+  });
+
+
+// $('[data-submenu]').submenupicker();
